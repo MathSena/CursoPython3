3f383f920f7a1aeadcf990198649c739ab35e828
MathSena <MathSena07@hotmail.com>
2021-02-06 20:32:57 -0300

    Criando repositório

---
+++ LógicaDeProgramação/aula1/comentarios.py
@@ -0,0 +1,11 @@
+# Isso é um comentário
+
+print('Linha 1')
+# O Python é uma linguagem intepretada
+print('Linha 2')  # Isso é um comentário
+print('linha 3')
+
+"""
+Esse é um comentário de múltiplas linhas
+
+"""
