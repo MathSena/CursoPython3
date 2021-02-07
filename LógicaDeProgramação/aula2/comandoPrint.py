35e690429eb5e7a71cd060300b8de9e565b3a82b
MathSena <MathSena07@hotmail.com>
2021-02-06 21:03:27 -0300

    Aula 2 - Comando Print

---
+++ LógicaDeProgramação/aula2/comandoPrint.py
@@ -0,0 +1,15 @@
+#  O comando print serve para mostrar informações na interface...print é uma função
+
+print(12345)
+print('Matheus', 'Sena')
+print('Matheus', 'Sena', sep='-', end = ' ')  # O Sep serve como um separador,e o end apresenta o conteúdo no final
+print('Matheus', sep = " - ", end = '//')
+
+"""
+Exercício da aula:
+Imprima o CPF no formato
+"""
+
+print("451","213","098", sep=".", end="-")
+print("56")
+
