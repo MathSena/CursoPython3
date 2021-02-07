ddef0274882ade1eab3e8ae353de32401e461589
MathSena <MathSena07@hotmail.com>
2021-02-06 22:00:57 -0300

    Aula 5 - Operadores

---
+++ LógicaDeProgramação/aula5/operadoresAritmeticos.py
@@ -0,0 +1,18 @@
+"""
+Os operadores são:
+
++
+-
+*
+/
+//
+**
+%
+
+"""
+
+print("Matheus nasceu em: ", (1900+96), " Ele possui ", (2020-1996), " Anos")
+print("Matheus Possui", (365*24) , " dias de vida")
+print("A média de idades entre Matheus e Esther é de: ", ((24+26)/2))
+print("2 elevado a 3 é: ", (2**3))
+
