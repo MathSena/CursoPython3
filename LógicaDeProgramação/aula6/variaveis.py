aca37d8f6e4a3781f5d1c52c35e6846ea2c43308
MathSena <MathSena07@hotmail.com>
2021-02-06 22:18:29 -0300

    Aula 6 - Variáveis

---
+++ LógicaDeProgramação/aula6/variaveis.py
@@ -0,0 +1,9 @@
+#Variável é um espaço na memória
+nome = "Matheus Sena"
+idade = 24
+altura = 1.70
+e_maior = idade >18
+peso = 55
+imc = (peso/(altura*altura))
+
+print(nome, " tem", idade, " e seu imc é de: ", imc)
