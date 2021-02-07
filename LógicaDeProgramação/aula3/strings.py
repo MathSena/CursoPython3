68eef61c64aba9a81372a5625e5a32db12725b17
MathSena <MathSena07@hotmail.com>
2021-02-06 21:10:46 -0300

    Aula 3 - Strings

---
+++ LógicaDeProgramação/aula3/strings.py
@@ -0,0 +1,7 @@
+#String é um tipo de dado primitivo -> str - string
+
+# Não funciona print(alguma coisa) se não houver declaração de variavél
+print('Alguma coisa')
+print("Alguma coisa")
+
+print("Essa é uma 'sting'")
