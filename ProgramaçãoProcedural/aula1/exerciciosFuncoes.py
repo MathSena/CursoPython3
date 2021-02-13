a7fd665a594964837a0f588541d92afe678bf735
MathSena <MathSena07@hotmail.com>
2021-02-13 20:53:18 -0300

    Exercícios de Funções em Python

--- ProgramaçãoProcedural/aula1/exerciciosFuncoes.py
+++ ProgramaçãoProcedural/aula1/exerciciosFuncoes.py
@@ -52,3 +52,41 @@ for i in range(100):
      aleatorio = randint(0, 100)
      print(fb(aleatorio))
 
+"""
+5 - Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da
+função2 executada.
+"""
+
+def ola_mundo():
+     return 'Olá mundo!'
+
+
+def mestre(funcao):
+     return funcao()
+
+
+print(ola_mundo())
+
+"""
+6 - Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da
+função2 executada. Faça a função1 executar duas funções que recebam um número 
+diferente de argumentos.
+"""
+
+
+def mestre(funcao, *args, **kwargs):
+    return funcao(*args, **kwargs)
+
+
+def fala_oi(nome):
+    return f'Oi {nome}'
+
+
+def saudacao(nome, saudacao):
+    return f'{saudacao} {nome}'
+
+
+executando = mestre(fala_oi, 'Luiz')
+executando2 = mestre(saudacao, 'Luiz', saudacao='Bom dia!')
+print(executando)
+print(executando2)
