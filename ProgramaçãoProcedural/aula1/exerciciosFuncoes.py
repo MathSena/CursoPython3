041432e987c8f3f0d3c6eefee7891579f9965295
MathSena <MathSena07@hotmail.com>
2021-02-13 20:42:40 -0300

    Exercícios de Funções em Python

---
+++ ProgramaçãoProcedural/aula1/exerciciosFuncoes.py
@@ -0,0 +1,54 @@
+"""
+1 - Crie uma função que exibe uma saudação com os parâmetros saudacao e nome.
+"""
+def saudacao(saudacao, nome):
+    print(saudacao, nome)
+saudacao("Bem-vindo", "Maguila")
+
+"""
+2 - Crie uma função que recebe 3 números como parâmetros e exiba a soma entre 
+eles.
+"""
+
+def somaTripla(n1,n2,n3):
+    print("A soma é: ", (n1+n2+n3))
+
+somaTripla(10,25,67)
+
+
+"""
+3 - Crie uma função que receba 2 números. O primeiro é um valor e o segundo um
+percentual (ex. 10%). Retorne (return) o valor do primeiro número somado
+do aumento do percentual do mesmo.
+"""
+
+def percentual(valor, porcento):
+    return valor + ((valor*porcento)/100)
+
+ap = percentual(100,10)
+print(ap)
+
+
+"""
+4 - Fizz Buzz - Se o parâmetro da função for divisível por 3, retorne fizz,
+se o parâmetro da função for divisível por 5, retorne buzz. Se o parâmetro da
+função for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o
+número enviado.
+"""
+
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return f'fizzbuzz, {n} é divisível por 3 e 5'
+    if n % 5 == 0:
+        return f'buzz, {n} é divisível por 5'
+    if n % 3 == 0:
+        return f'fizz, {n} é divisível por 3'
+    return n
+
+
+from random import randint
+
+for i in range(100):
+     aleatorio = randint(0, 100)
+     print(fb(aleatorio))
+
