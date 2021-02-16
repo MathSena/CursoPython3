d25fa5d7d5658f4f7ee8b03b186dc6a2cbd9deaf
MathSena <MathSena07@hotmail.com>
2021-02-15 21:03:35 -0300

    Tuplas em Python

--- ProgramaçãoProcedural/aula3/tuplas.py
+++ ProgramaçãoProcedural/aula3/tuplas.py
@@ -1,4 +1,4 @@
-#Não consigo alterar elementos de tuplas
+#Não consigo  alterar elementos de tuplas
 
 t1 = (1,2,3, 'a', 'Luiz Otávio')
 
@@ -16,6 +16,7 @@ print('Tupla antes de alterar valor: ', t2)
 t2[1] = 3000
 t2 = tuple(t2)
 print('Tupla depois de  alterar valor: ', t2)
+
 
 
 
