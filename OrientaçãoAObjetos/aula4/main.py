47c55f33e456040892b3676baaf729de80a8fb10
MathSena <MathSena07@hotmail.com>
2021-02-25 01:12:22 -0300

    Encapsulamento em Python

--- OrientaçãoAObjetos/aula4/main.py
+++ OrientaçãoAObjetos/aula4/main.py
@@ -1,17 +1,25 @@
 """
 public, protected e private
+_ -> protected
+__ ->private
 """
 
 class BaseDados:
     def __init__(self): #Similiar a Construtor
-        self.dados = {}
-
+        self._dados = {}
 
     def inserirDados(self, id, nome):
-        if 'clientes' not in self.dados:
-            self.dados['clientes'] = {id: nome}
+        if 'clientes' not in self._dados:
+            self._dados['clientes'] = {id: nome}
         else:
-            self.dados['clientes'].update({id: nome})
+            self._dados['clientes'].update({id: nome})
+
+    def listaCLientes(self):
+        for id, nome in self._dados['clientes'].itens():
+            print(id, nome)
+
+    def apagaCliente(self, id):
+        del self._dados['clientes'][id]
 
 
 
@@ -20,4 +28,4 @@ bd = BaseDados()
 bd.inserirDados(1, 'Matheus')
 bd.inserirDados(2, 'Juliette')
 bd.inserirDados(3, 'Gil')
-print(bd.dados)
+print(bd._dados)
