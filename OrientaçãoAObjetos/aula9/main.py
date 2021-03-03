b9a019d528babc9257a76c90535be266b6fc1ffb
MathSena <MathSena07@hotmail.com>
2021-03-02 22:48:36 -0300

    Sobreposição de membros em Python

---
+++ OrientaçãoAObjetos/aula9/main.py
@@ -0,0 +1,22 @@
+from classes import *
+
+"""
+Associação - > Usa
+Agregação -> Tem
+Composição -> Dono
+Herança -> É
+"""
+
+#c1 = Cliente('Sarah', 28)
+#c1.falar()
+#c1.comprar()
+# c1.estudar()
+
+#a1 = Aluno('Juliette', 31)
+#a1.falar()
+#a1.estudar()
+# a1.comprar()
+
+d1 = ClienteVIP('Gil', 31)
+d1.comprar()
+d1.falar()
