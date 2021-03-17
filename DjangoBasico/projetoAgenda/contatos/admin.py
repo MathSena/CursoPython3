1e22e3037a2a470309f54f2c2a80b86b8fcd0fac
MathSena <MathSena07@hotmail.com>
2021-03-16 22:19:50 -0300

    Exibindo valores nas views

--- DjangoBasico/projetoAgenda/contatos/admin.py
+++ DjangoBasico/projetoAgenda/contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome', 'telefone', 'email')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'telefone', 'email')
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- DjangoBasico/projetoAgenda/contatos/models.py
+++ DjangoBasico/projetoAgenda/contatos/models.py
@@ -6,6 +6,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -14,4 +17,7 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.CharField(max_length=255, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
 
