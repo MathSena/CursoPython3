25a5a4455c87177d4e9a865eb02a0eb6b81997f4
MathSena <MathSena07@hotmail.com>
2021-03-20 16:08:08 -0300

    Adicionando campo de busca

--- DjangoBasico/projetoAgenda/contatos/views.py
+++ DjangoBasico/projetoAgenda/contatos/views.py
@@ -1,11 +1,15 @@
+from django.core import paginator
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 # Create your views here.
 def index(request):
     contatos = Contato.objects.order_by('nome')
-    paginator = Paginator(contatos, 1)  # Show 20 contacts per page
+    paginator = Paginator(contatos, 20)  # Show 20 contacts per page
 
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -17,10 +21,31 @@ def index(request):
 def ver_contato(request, contato_id):
     #contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
     return render(request, 'contatos/ver_contato.html',{
         'contato': contato
 
     })
 
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None:
+        return Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains =termo)| Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos,20)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html',{
+        'contatos': contatos
+
+    })
 
 
