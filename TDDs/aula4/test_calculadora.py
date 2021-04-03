c81033aabd63b2340c995f2c3135f240bf257b65
MathSena <MathSena07@hotmail.com>
2021-04-02 21:47:59 -0300

    Unittest #2 - Com TDD

--- TDDs/aula4/test_calculadora.py
+++ TDDs/aula4/test_calculadora.py
@@ -9,5 +9,19 @@ class TestCalculadora(unittest.TestCase):
     def test_soma_5_negativo_e_5_deve_retonar_0(self):
         self.assertEqual(soma(-5, 5), 0)
 
+    def test_soma_varias_entradas(self):
+        x_y_saidas = (
+            (10, 10, 20),
+            (5, 5, 10),
+            (1.5, 1.5, 3.0),
+            (-5, 5, 0),
+            (100, 100, 200),
+        )
+
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x, y), saida)
+
 
 unittest.main(verbosity=2)
